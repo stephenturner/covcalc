26a2e0c89ef650f66d1a32cd8cfeb2405b6302b1
Stephen Turner <vustephen@gmail.com>
2016-06-01 13:32:03 -0400

    properly implemented the other box

--- app.R
+++ app.R
@@ -41,14 +41,20 @@ ui <- fluidPage(
                      choices = list("Paired-end" = 2, "Single-end" = 1),
                      selected = 2),
 
-        numericInput("genomesize", label = h4("Genome size (bp)"), value = 3.2e9),
-        p("Haploid genome size. Can use scientific notation (e.g., \"3.2e9\" = 3,200,000,000). Examples:"),
-        HTML("<ul>
-             <li>Human genome: 3.2e9</li>
-             <li>Agilent V6 exome: 60e6</li>
-             <li>E. coli K-12: 4.6e6</li>
-             <li>HIV: 9.2e3</li>
-             </ul>")
+
+
+        HTML("<h4>Genome size</h4><p>Pick a genome below or manually enter the haploid genome size in bp. You can use scientific notation (e.g., enter <strong>3.2e9</strong> for 3,200,000,000bp, or 3.2 Gb).</p>"),
+
+        radioButtons("genomesizebutton", label=NULL,
+                     choices=list(
+                       "Human (3.1 Gb)" = 3096649726,
+                       "Agilent V6 exome (60 Mb)" = 60e6,
+                       "S. cerevisiae (12.2 kb)" = 12157105,
+                       "E. coli K-12 (4.6 Mb)" = 	4641652,
+                       "Other (Enter manually)"=""),
+                     selected=3096649726),
+
+        uiOutput("genomesizeOtherUI")
 
       ),
 
@@ -81,6 +87,29 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
+  observe({
+    if (input$genomesizebutton!="") {
+      output$genomesizeOtherUI <- renderText(
+        HTML("Selected genome size: <strong>", prettyNum(input$genomesizebutton, big.mark=","), "</strong>")
+      )
+    } else {
+      output$genomesizeOtherUI <- renderUI({
+        numericInput("genomesizeOther", label=NA, value=NULL)
+      })
+    }
+
+  })
+
+  genomesize <- reactive({
+    if (input$genomesizebutton!="") {
+      return(as.numeric(input$genomesizebutton))
+    } else {
+      return(input$genomesizeOther)
+    }
+  })
+
+  # observe(print(genomesize()))
+
   output$ui <- renderUI({
     if (is.null(input$whatcalc)) {
       return()
@@ -97,14 +126,14 @@ server <- function(input, output) {
 
   # Converts genome size into SI-prefix character
   bptosi <- function(bp) {
-    if (bp>1e9)      return(paste0(round(bp/1e9, 3), "GB"))
-    else if (bp>1e6) return(paste0(round(bp/1e6, 3), "MB"))
-    else if (bp>1e3) return(paste0(round(bp/1e3, 3), "KB"))
+    if (bp>1e9)      return(paste0(round(bp/1e9, 2), "GB"))
+    else if (bp>1e6) return(paste0(round(bp/1e6, 2), "MB"))
+    else if (bp>1e3) return(paste0(round(bp/1e3, 2), "KB"))
     else             return(paste0(bp, "bp"))
   }
 
   outputtext <- reactive({
-    req(input$whatcalc)
+    req(input$whatcalc, genomesize)
     if (input$whatcalc=="reads") {
       paste0("Calculating reads", input$dynamic)
     } else if (input$whatcalc=="coverage") {
@@ -114,21 +143,22 @@ server <- function(input, output) {
 
   output$text <- renderText({
     req(input$whatcalc)
+    if (input$genomesizebutton=="") req(input$genomesizeOther)
     if (input$whatcalc=="reads") {
       # N=CG/L
-      reads <- (as.numeric(input$dynamic)*as.numeric(input$genomesize))/(as.numeric(input$readlength)*as.numeric(input$sepe))
-      paste0("*", signif(reads/1e6, 3), " million reads* required for ", input$dynamic, "X coverage of a ", bptosi(input$genomesize), " genome using ", input$sepe, "x", input$readlength, " sequencing reads.")
+      reads <- (as.numeric(input$dynamic)*as.numeric(genomesize()))/(as.numeric(input$readlength)*as.numeric(input$sepe))
+      paste0("*", signif(reads/1e6, 3), " million reads* required for ", input$dynamic, "X coverage of a ", bptosi(genomesize()), " genome using ", input$sepe, "x", input$readlength, " sequencing reads.")
     } else if (input$whatcalc=="coverage") {
       # C=LN/G
-      coverage <- (as.numeric(input$readlength)*as.numeric(input$sepe))*as.numeric(input$dynamic)*1e6/as.numeric(input$genomesize)
-      paste0("*", round(coverage), "X coverage* for a ", bptosi(input$genomesize), " genome obtained with ", input$dynamic, "M ", input$sepe, "x", input$readlength, " sequencing reads.")
+      coverage <- (as.numeric(input$readlength)*as.numeric(input$sepe))*as.numeric(input$dynamic)*1e6/as.numeric(genomesize())
+      paste0("*", round(coverage), "X coverage* for a ", bptosi(genomesize()), " genome obtained with ", input$dynamic, "M ", input$sepe, "x", input$readlength, " sequencing reads.")
     }
   })
 
-  output$myplot <- renderPlot({
-    req(input$dynamic)
-    plot(input$dynamic)
-  })
+  # output$myplot <- renderPlot({
+  #   req(input$dynamic)
+  #   plot(input$dynamic)
+  # })
 
    # output$distPlot <- renderPlot({
    #    # generate bins based on input$bins from ui.R
